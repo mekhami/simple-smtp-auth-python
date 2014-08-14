6d36e8f1e0281253eedc778f798596117f9b4ba0
mekhami <lawrence.vanderpool@gmail.com>
2014-08-14 11:10:45 -0500

    cleanup

--- smtpauth.py
+++ smtpauth.py
@@ -16,11 +16,11 @@ if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--server", action="store", dest='server', help="server address")
-	parser.add_argument("--port", help="server port", dest='port', type=int)
+	parser.add_argument("--port", action="store", dest='port', help="server port", type=int)
 	parser.add_argument("--username", action="store", dest='username', help="username")
 	parser.add_argument("--password", action="store", dest='password', help="password")
 
 	results = parser.parse_args()
-	#print results
+	
 	print is_authenticated(results.server, results.port, results.username, results.password)
 
