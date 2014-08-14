14d751c6ea0153b257759daf3f5b5ff3fed3540e
mekhami <lawrence.vanderpool@gmail.com>
2014-08-14 10:56:43 -0500

    Added Argparse functionality

--- smtpauth.py
+++ smtpauth.py
@@ -1,5 +1,5 @@
 import smtplib
-import sys
+import sys, argparse
 
 def is_authenticated(server, port, username, password):
 	s = smtplib.SMTP(server, port)
@@ -14,10 +14,13 @@ def is_authenticated(server, port, username, password):
 
 if __name__ == '__main__':
 
-	server = sys.argv[1]
-	username = sys.argv[2]
-	password = sys.argv[3]
-	port = sys.argv[4]
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--server", action="store", dest='server', help="server address")
+	parser.add_argument("--port", help="server port", dest='port', type=int)
+	parser.add_argument("--username", action="store", dest='username', help="username")
+	parser.add_argument("--password", action="store", dest='password', help="password")
 
-	print is_authenticated(server, port, username, password)
+	results = parser.parse_args()
+	#print results
+	print is_authenticated(results.server, results.port, results.username, results.password)
 
